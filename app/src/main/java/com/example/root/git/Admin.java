860b3719f9dd510160b26f0fe0b9d33fde4c619d
sufiyan1122 <sufiyan11221122@gmail.com>
2017-03-12 13:07:33 +0530

    removed load method

--- app/src/main/java/com/example/root/git/Admin.java
+++ app/src/main/java/com/example/root/git/Admin.java
@@ -9,8 +9,7 @@ public class Admin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-        loadPage();
 
     }
-    
+
 }
